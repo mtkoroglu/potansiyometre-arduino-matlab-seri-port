1508865cc799613290c26e16b74477fa7e37033f
taha-koroglu <m.tahakoroglu@gmail.com>
2020-12-18 01:43:54 +0300

    Update potansiyometre.m

--- potansiyometre.m
+++ potansiyometre.m
@@ -1,20 +1,19 @@
 clear all; close all; clc;
-
 s = serialport('COM7', 57600); flush(s);
-kanal = uint8([0, 0, 0, 0]);
-i = 0; % paket
+kanal = uint8(zeros(1,4));
+i = 0; % paket numarası
+n = 4; % kanal sayısı
 stopTime = 15; % saniye
-tic
+tic;
 while ( true )
     zaman = toc;
     if ( read(s, 1, 'uint8') == 'h' )
         i = i + 1; % paket alınıyor
-        kanal(1) = read(s, 1, 'uint8');
-        kanal(2) = read(s, 1, 'uint8');
-        kanal(3) = read(s, 1, 'uint8');
-        kanal(4) = read(s, 1, 'uint8');
+        for j=1:n
+            kanal(i) = read(s, 1, 'uint8');
+        end
     end
-    fprintf('Paket#1 = %i    Kanal 1 = %i    Kanal 2 = %.i    Kanal 3 = %i    Kanal 4 = %i    zaman = %.2f\n', ...
+    fprintf('Paket#1 = %i  Kanal 1 = %i  Kanal 2 = %.i  Kanal 3 = %i  Kanal 4 = %i  Zaman = %.2f\n', ...
         i, kanal(1), kanal(2), kanal(3), kanal(4), zaman);
     if (zaman > stopTime)
         break;
